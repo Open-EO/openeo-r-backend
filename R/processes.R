72cf9a0f83231986aaef49b7971b5b514a5eaee7
Florian Lahn <florian.lahn@uni-muenster.de>
2018-01-04 17:49:47 +0100

    added a process to find the minimum value in a single band collection

--- R/processes.R
+++ R/processes.R
@@ -28,6 +28,7 @@ registerProcesses = function() {
       #imagery might be an identifier or a function (Process$execute()) or a json process description or a
       # udf or a collection we need to specify that
       collection = NULL
+      
       if ("Product" %in% class(imagery)) {
         collection = imagery$getCollection()
       } else if ("Collection" %in% class(imagery)) {
@@ -66,12 +67,68 @@ registerProcesses = function() {
   #crop_extent = Process$new()
   #crop_extent$register()
   
-  #find_min = Process$new()
-  #find_min$register()
+  find_min = Process$new(
+    process_id = "find_min",
+    description = "calculates the minimum value of a single valued band collection",
+    args = list(
+      Argument$new(
+        name = "imagery",
+        description = "the temporal dataset/collection",
+        required = TRUE
+      )
+    ),
+    operation = function(imagery) {
+      #get the collection of the imagery
+      collection = getCollectionFromImageryStatement(imagery)
+      
+      #get a list of the data (raster objects)
+      rasters = lapply(collection$granules, function(obj){obj$data})
+      
+      #create a brick
+      data = brick(rasters)
+      
+      #calculate
+      minimum = calc(data,fun=min,na.rm=T)
+      
+      #create a granule
+      aggregation = Granule$new(time=collection$getMinTime(),data=minimum,extent=extent(minimum),srs=crs(minimum))
+      
+      #create a collection
+      collection = Collection$new()
+      collection$addGranule(aggregation)
+      collection$sortGranulesByTime
+      
+      return(collection)
+    }
+  )
+  
+  find_min$register()
   
   #calculate_ndvi = Process$new()
   #calculate_ndvi$register()
   
   
+}
+
+getCollectionFromImageryStatement = function (imagery) {
+  collection = NULL
+  
+  if ("Product" %in% class(imagery)) {
+    collection = imagery$getCollection()
+  } else if ("Collection" %in% class(imagery)) {
+    collection = imagery
+  } else if (class(imagery) == "character") {
+    #load image or create process
+  } else if ("Process" %in% class(imagery)) {
+    collection = imagery$execute()
+  } else if (class(imagery) == "list") {
+    if ("product_id" %in% names(imagery)) {
+      collection = openeo$data[[imagery$product_id]]$getCollection()
+    }
+  }
+  if (is.null(collection)) {
+    stop("no collection element found in function call")
+  }
+  return (collection)
 }
 
