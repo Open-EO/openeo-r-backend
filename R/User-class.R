df064f928d660546c73004f5355add2cc150b60f
Florian Lahn <florian.lahn@uni-muenster.de>
2018-01-09 11:08:19 +0100

    added the a basic User class

---
+++ R/User-class.R
@@ -0,0 +1,33 @@
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+User <- R6Class(
+  "User",
+  public = list(
+    user_id = NULL,
+    workspace = NULL,
+    files = NULL,
+    jobs = NULL,
+    user_name = NULL,
+    password = NULL,
+    
+    initialize = function(user_id) {
+      self$user_id = user_id
+    },
+    
+    toList = function() {
+      return(
+        list(
+          user_id = self$user_id,
+          user_name = self$user_name,
+          password = self$password,
+          jobs = self$jobs
+        )
+      )
+    }
+  )
+)
+
+isUser = function(obj) {
+  return(all("User" %in% class(obj)))
+}
