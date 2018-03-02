1a87a1e40b1c57ad55bb0815654d50338453d762
Matthias Mohr <m.mohr@uni-muenster.de>
2018-03-02 13:27:58 +0100

    Sending a specific origin in the header for CORS and authentication. Allow-Credentials value must be lower case. Don't require authentication with OPTIONS calls.

--- R/api.R
+++ R/api.R
@@ -174,6 +174,9 @@ openeo.server$api.version <- "0.0.2"
 
 #* @filter checkAuth
 .authorized = function(req, res){
+  if (req$REQUEST_METHOD == 'OPTIONS') {
+    return(forward())
+  }
   tryCatch({
     auth = unlist(strsplit(req$HTTP_AUTHORIZATION," "))
     if (auth[1] == "Bearer") {
@@ -201,17 +204,16 @@ openeo.server$api.version <- "0.0.2"
   )
 }
 
-.cors_filter = function(res) {
-  res$setHeader("Access-Control-Allow-Origin", "*")
-  res$setHeader("Access-Control-Allow-Credentials", TRUE)
+.cors_filter = function(req,res) {
+  res$setHeader("Access-Control-Allow-Origin", req$HTTP_ORIGIN)
+  res$setHeader("Access-Control-Allow-Credentials", "true")
   plumber::forward()
 }
 
 .cors_option_bypass = function(req,res, ...) {
   res$setHeader("Access-Control-Allow-Headers", "Authorization, Accept, Content-Type")
   res$setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,PATCH")
-  
-  return(res)
+  ok(res);
 }
 
 .not_implemented_yet = function(req,res, ...) {
