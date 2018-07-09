0560cbdc9f7c1bb151c3f9004f40d283d8b99855
Florian Lahn <florian.lahn@uni-muenster.de>
2018-07-09 12:49:20 +0200

    functions for exporting Collection data for UDF processing webservice
    * moved udf export function into prepare_UDF
    * changed the behavior of the export function that it will be applied to grouped data and is possibly used also for all types of collection data
    * added additionally metadata (not all correct, e.g. wavelength what is currently the band_index)
    * added a crs() function for Collection to access the spatial reference system easier

--- R/prepare_UDF.R
+++ R/prepare_UDF.R
@@ -19,3 +19,89 @@ write_generics = function(collection_obj, dir_name = "disk") #dir_name could be 
   R2G_obj$legend_to_disk(dir_name)
 
 }
+
+#' Prepares the collection data for the UDF service request
+#' 
+#' Transforms the data contained in a Collection into a JSON representation. It will be passed along the code script URL as data
+#' to the specified UDF REST processing service. Currently implemented only for raster timeserires collections.
+#' 
+#' @param collection Collection object
+#' @return list that can be transformed into "UdfData" JSON
+#' @export
+udf_export = function(collection) {
+  if (! is.Collection(collection)) {
+    stop("Passed object is not a Collection object")
+  }
+  
+  # TODO prepare some sort of tiling strategy
+  
+  if (collection$dimensions$raster && collection$dimensions$space && collection$dimensions$time) {
+    udf_data = list()
+    udf_data[["proj"]] = as.character(collection$getGlobalSRS())
+    udf_data[["raster_collection_tiles"]] = list()
+    
+    udf_data[["raster_collection_tiles"]] = append(udf_data[["raster_collection_tiles"]],raster_collection_export(collection))
+    return(udf_data)
+  } else {
+    stop("Not yet implemented")
+  }
+  
+}
+
+#' Creates RasterCollectionTile representation
+#' 
+#' Subsets and groups Collection data by band and space in order to create the specified UDF RasterCollectionTile JSON output.
+#' 
+#' @param collection Collection object
+#' @return list that can be transformed into "UdfData" JSON
+#' @export
+raster_collection_export = function(collection) {
+  if (! is.Collection(collection)) {
+    stop("Passed object is not a Collection object")
+  }
+  
+  data = collection$getData()
+  extents = collection$space
+  
+  modified = data %>% group_by(band,space) %>% dplyr::summarise(
+    exported = tibble(band,space,data,time) %>% (function(x,...) {
+      raster_collection_tiles = list()
+      raster_collection_tiles[["id"]] = "test1"
+      
+      raster_collection_tiles[["wavelength"]] = unique(x[["band"]])
+      
+      # select sf polygons by ids stored in the data table, then give bbox from all of the sf
+      b = st_bbox(extents[x %>% dplyr::select(space) %>% unique() %>% unlist() %>% unname(),])
+      raster_collection_tiles[["extent"]] = list(
+        north = b[["ymax"]],
+        south = b[["ymin"]],
+        west = b[["xmin"]],
+        east = b[["xmax"]],
+        height = yres(x[[1,"data"]]),
+        width = xres(x[[1,"data"]])
+      )
+      
+      # times
+      times = x[["time"]]
+      tres = round(median(diff(times)))
+      raster_collection_tiles[["start_times"]] = strftime(times, "%Y-%m-%dT%H:%M:%S", usetz = TRUE)
+      raster_collection_tiles[["end_times"]] = strftime(times+tres, "%Y-%m-%dT%H:%M:%S", usetz = TRUE)
+      
+      # fetch data from raster files as matrix (store as list first other wise it messes up the matrix 
+      # structure by creating row/col as rows for each attribute)
+      raster_collection_tiles[["data"]] = x %>% apply(MARGIN=1,FUN= function(row) {
+        
+        return(list(raster::values(x=row$data, format="matrix")))
+        
+      })
+      
+      # unlist it again
+      raster_collection_tiles[["data"]] = lapply(raster_collection_tiles[["data"]], function(arr_list) {
+        arr_list[[1]]
+      })
+      
+      return(list(raster_collection_tiles))
+    })
+  )
+  return(modified[["exported"]])
+}
