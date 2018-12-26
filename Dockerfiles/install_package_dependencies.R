59d09c29f88b3ba9f6dc04182e1232f083393478
Pramit Ghosh <32305898+pramitghosh@users.noreply.github.com>
2018-12-26 17:38:43 +0100

    Update install_package_dependencies.R

--- Dockerfiles/install_package_dependencies.R
+++ Dockerfiles/install_package_dependencies.R
@@ -3,10 +3,11 @@ cran.mirror = "https://cran.uni-muenster.de/"
 install.packages("devtools",repos=cran.mirror)
 library(devtools)
 
-install_deps("/opt/dockerfiles",repos=cran.mirror)
-installation = list.files("/opt/dockerfiles",recursive=TRUE,full.names = TRUE)
+# install_deps("/opt/dockerfiles",repos=cran.mirror)
+# installation = list.files("/opt/dockerfiles",recursive=TRUE,full.names = TRUE)
+install.packages(c("rgdal", "gdalUtils", "raster", "sodium", "plumber", "DBI", "RSQLite", "future", "httr", "httpuv", "tibble", "dplyr", "rgeos", "magrittr", "units", "sf", "lubridate"), repos = cran.mirror)
 
 # packages for the R UDFs
 install_github("r-spatial/stars", dependencies=TRUE)
 
-removed = file.remove(installation)
+# removed = file.remove(installation)
