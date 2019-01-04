f0e186da65cd7e0c8730bcd74acc753f291a5c93
Pramit Ghosh <32305898+pramitghosh@users.noreply.github.com>
2019-01-04 01:54:07 +0100

    Install latest openEO.R.UDF from Github

--- Dockerfiles/install_package.R
+++ Dockerfiles/install_package.R
@@ -1,7 +1,7 @@
 library(devtools)
 
 # install the R UDF package from github
-install_github("pramitghosh/OpenEO.R.UDF",ref="v0.0.1",dependencies=TRUE)
+install_github("pramitghosh/openEO.R.UDF", dependencies = TRUE)
 
 # install the R back-end package
 install("/opt/dockerfiles")
