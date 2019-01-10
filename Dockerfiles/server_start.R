c9c9c7272944aff8c4c7cc63dadc59a5554eb98c
Florian Lahn <florian.lahn@uni-muenster.de>
2019-01-10 16:24:54 +0100

    fixes #21  bad docker server_start script

--- Dockerfiles/server_start.R
+++ Dockerfiles/server_start.R
@@ -2,11 +2,12 @@ library(openEO.R.Backend)
 
 config = ServerConfig()
 config$workspaces.path = "/var/openeo/workspace"
-config$initEnvironmentDefault()
-config$initializeDatabase()
+
 
 createServerInstance(configuration = config)
 
+openeo.server$initEnvironmentDefault()
+openeo.server$initializeDatabase()
 openeo.server$createUser(user_name="test", password="test") #only created if not exists
 openeo.server$loadDemo()
 openeo.server$startup(port = 8000,host="0.0.0.0")
